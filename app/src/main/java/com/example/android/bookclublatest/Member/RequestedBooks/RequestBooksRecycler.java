f6db561252134c7391835cb0b7a5041e6597f3e1
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-08-11 16:23:46 +0530

    requested book done

--- app/src/main/java/com/example/android/bookclublatest/Member/RequestedBooks/RequestBooksRecycler.java
+++ app/src/main/java/com/example/android/bookclublatest/Member/RequestedBooks/RequestBooksRecycler.java
@@ -2,24 +2,35 @@ package com.example.android.bookclublatest.Member.RequestedBooks;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.android.bookclublatest.HomePage.RequestBook.RequestModel;
 import com.example.android.bookclublatest.R;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.TimeZone;
 
 public class RequestBooksRecycler extends RecyclerView.Adapter<RequestBooksRecycler.ViewHolder> {
     List<RequestModel> list;
     Context context;
+    RequestModel model;
+    final static String[] months={"Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct","Nov","Dec"};
+
 
     public RequestBooksRecycler(List<RequestModel> list, Context context) {
         this.list = list;
@@ -36,7 +47,7 @@ public class RequestBooksRecycler extends RecyclerView.Adapter<RequestBooksRecyc
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull RequestBooksRecycler.ViewHolder viewHolder, int i) {
-        RequestModel model = list.get(i);
+         model = list.get(i);
         if(model.getPubl().isEmpty())
             viewHolder.name.setText(model.getBook() + ", "+model.getAuthor());
         else
@@ -46,6 +57,7 @@ public class RequestBooksRecycler extends RecyclerView.Adapter<RequestBooksRecyc
         viewHolder.info.setText(model.getInfo());
         viewHolder.user.setText(model.getEmail());
         viewHolder.addedon.setText("Added on "+model.getApproval_date());
+        viewHolder.date.setText("Date: "+ model.getRequested_date());
         if (model.getInfo().isEmpty())
             viewHolder.info.setText("Additional Info Not Provided");
         if(model.getStatus().equals("Pending")){
@@ -57,6 +69,34 @@ public class RequestBooksRecycler extends RecyclerView.Adapter<RequestBooksRecyc
             viewHolder.deny.setVisibility(View.GONE);
             viewHolder.greenbar.setVisibility(View.VISIBLE);
         }
+        viewHolder.done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference idRef = FirebaseDatabase.getInstance().getReference("Request Book").child(model.getId());
+                Calendar calendar2=Calendar.getInstance(TimeZone.getDefault());
+                final String current=calendar2.get(Calendar.DAY_OF_MONTH) + " " + months[(calendar2.get(Calendar.MONTH))]
+                        + "," + calendar2.get(Calendar.YEAR);
+                idRef.child("approval_date").setValue(current);
+                idRef.child("status").setValue("Approved");
+                notifyDataSetChanged();
+
+            }
+        });
+        viewHolder.deny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Deleting request",Toast.LENGTH_SHORT).show();
+                DatabaseReference idRef = FirebaseDatabase.getInstance().getReference("Request Book").child(model.getId());
+                idRef.removeValue(new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                        Toast.makeText(context,"Request denied",Toast.LENGTH_SHORT).show();
+                        notifyDataSetChanged();
+
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -65,7 +105,7 @@ public class RequestBooksRecycler extends RecyclerView.Adapter<RequestBooksRecyc
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView name, author, publ, info, user , addedon;
+        TextView name, author, publ, info, user , addedon, date;
         RelativeLayout done,deny;
         ConstraintLayout greenbar;
 
@@ -80,6 +120,8 @@ public class RequestBooksRecycler extends RecyclerView.Adapter<RequestBooksRecyc
             deny = itemView.findViewById(R.id.deny_button);
             addedon = itemView.findViewById(R.id.requestapprovedon);
             greenbar = itemView.findViewById(R.id.greenbar);
+            date = itemView.findViewById(R.id.request_date);
+
         }
     }
 }
