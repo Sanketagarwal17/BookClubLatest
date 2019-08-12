29795da1e839a6d3350f76d06ddc8478fc0c56fc
rishabh-997 <rishabh.agarwal997@gmail.com>
2019-08-12 20:32:01 +0530

    issue book resolved for multiple copies

--- app/src/main/java/com/example/android/bookclublatest/Member/ConfirmIssue/ConfirmIssueModel.java
+++ app/src/main/java/com/example/android/bookclublatest/Member/ConfirmIssue/ConfirmIssueModel.java
@@ -2,13 +2,14 @@ package com.example.android.bookclublatest.Member.ConfirmIssue;
 
 public class ConfirmIssueModel
 {
-    String email,isbn,name,date;
+    String email,isbn,name,date,ism;
 
-    public ConfirmIssueModel(String email, String isbn, String name, String date) {
+    public ConfirmIssueModel(String email, String isbn, String name, String date,String ism) {
         this.email = email;
         this.isbn = isbn;
         this.name = name;
         this.date = date;
+        this.ism = ism;
     }
 
     public String getDate() {
@@ -25,5 +26,9 @@ public class ConfirmIssueModel
 
     public String getName() {
         return name;
+    }
+
+    public String getIsm() {
+        return ism;
     }
 }
