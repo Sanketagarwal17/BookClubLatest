92636189170798dca31f605002092da9bce66fed
akonic <ankitkachhi.ak47@gmail.com>
2019-09-01 23:21:00 +0530

    confirm issue

--- app/src/main/java/com/example/android/bookclublatest/Member/ConfirmIssue/ConfirmIssueActivity.java
+++ app/src/main/java/com/example/android/bookclublatest/Member/ConfirmIssue/ConfirmIssueActivity.java
@@ -10,10 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -63,6 +66,26 @@ public class ConfirmIssueActivity extends AppCompatActivity implements ConfirmIs
 
         title = (TextView) findViewById(R.id.textView26);
         title.setText("ISSUE REQUESTS");
+        EditText editText= findViewById(R.id.searchbox2);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+
+            }
+        });
+
+
 
         FloatingActionButton fab=findViewById(R.id.btn_sort);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -108,7 +131,7 @@ public class ConfirmIssueActivity extends AppCompatActivity implements ConfirmIs
 
     }
 
-   
+
 
 
     private void showSortDialog() {
@@ -146,6 +169,15 @@ public class ConfirmIssueActivity extends AppCompatActivity implements ConfirmIs
         }
         adapter=new ConfirmIssueAdapter(list,ConfirmIssueActivity.this,ConfirmIssueActivity.this);
         recyclerView.setAdapter(adapter);
+    }
+    private void filter(String text){
+        ArrayList<ConfirmIssueModel> filteredList = new ArrayList<>();
+        for (ConfirmIssueModel item:list){
+            if(item.getName().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+        adapter.filterList(filteredList);
     }
 
     @Override
